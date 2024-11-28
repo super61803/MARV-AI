ca18a3894b0909c61f196aaa03a18d716ed4e9be
Super <newest.star1990@gmail.com>
2024-11-28 01:22:43 -0500

    feat: added types

---
+++ types/index.d.ts
@@ -0,0 +1,17 @@
+// StyleSheets
+declare module "*.css";
+
+// Images
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.webp";
+declare module "*.bmp";
+
+declare module "*.js";
+declare module "*.json";
+
+// Import Alias
+declare module "@/*";
