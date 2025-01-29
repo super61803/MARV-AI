2e09f00949ca4e56d7c1651ca43dad3a033479a1
Super <newest.star1990@gmail.com>
2025-01-28 21:27:08 -0500

    chore: update eslint configuration, Dockerfile, and package.json

--- src/payload/collections/Users/index.ts
+++ src/payload/collections/Users/index.ts
@@ -43,193 +43,285 @@ const Users: CollectionConfig = {
   ],
   fields: [
     {
-      name: 'name',
-      type: 'text',
-      required: true,
-    },
-    {
-      name: 'firstName',
-      type: 'text',
-      required: true,
-    },
-    {
-      name: 'lastName',
-      type: 'text',
-      required: true,
-    },
-    {
-      name: 'email',
-      type: 'email',
-      unique: true,
-      required: true,
-    },
-    {
-      name: 'roles',
-      type: 'select',
-      hasMany: true,
-      defaultValue: ['marveler'],
-      options: [
+      type: 'tabs',
+      tabs: [
         {
-          label: 'admin',
-          value: 'admin',
+          label: 'Personal Information',
+          fields: [
+            {
+              name: 'name',
+              type: 'text',
+              required: true,
+            },
+            {
+              name: 'firstName',
+              type: 'text',
+              required: true,
+            },
+            {
+              name: 'lastName',
+              type: 'text',
+              required: true,
+            },
+            {
+              name: 'email',
+              type: 'email',
+              unique: true,
+              required: true,
+            },
+            {
+              name: 'roles',
+              type: 'select',
+              hasMany: true,
+              defaultValue: ['marveler'],
+              options: [
+                {
+                  label: 'admin',
+                  value: 'admin',
+                },
+                {
+                  label: 'Marveler',
+                  value: 'marveler',
+                },
+                {
+                  label: 'Counselor',
+                  value: 'counselor',
+                },
+                {
+                  label: 'Manager',
+                  value: 'manager',
+                },
+                {
+                  label: 'Support',
+                  value: 'support',
+                },
+              ],
+              hooks: {
+                beforeChange: [ensureFirstUserIsAdmin],
+              },
+              access: {
+                read: admins,
+                create: admins,
+                update: admins,
+              },
+            },
+            {
+              name: 'dob',
+              type: 'date',
+              required: true,
+            },
+            {
+              name: 'phoneCode',
+              type: 'number',
+              required: true,
+            },
+            {
+              name: 'phoneNumber',
+              type: 'text',
+              required: true,
+            },
+            {
+              name: 'profile_img',
+              type: 'relationship',
+              relationTo: 'media',
+              required: true,
+            },
+            {
+              name: 'gender',
+              type: 'radio',
+              options: [
+                {
+                  label: 'Male',
+                  value: 'male',
+                },
+                {
+                  label: 'Female',
+                  value: 'female',
+                },
+                {
+                  label: 'Other',
+                  value: 'other',
+                },
+              ],
+              required: true,
+            },
+            {
+              name: 'nationality',
+              type: 'relationship',
+              relationTo: 'countries',
+              hasMany: false,
+              required: false,
+            },
+          ],
         },
         {
-          label: 'Marveler',
-          value: 'marveler',
+          label: 'Contact Information',
+          fields: [
+            {
+              name: 'city',
+              type: 'text',
+              required: false,
+            },
+            {
+              name: 'years_in_city',
+              type: 'number',
+              required: false,
+            },
+            {
+              name: 'facebook',
+              type: 'text',
+              required: false,
+            },
+            {
+              name: 'twitter',
+              type: 'text',
+              required: false,
+            },
+            {
+              name: 'instagram',
+              type: 'text',
+              required: false,
+            },
+            {
+              name: 'linkedIn',
+              type: 'text',
+              required: false,
+            },
+          ],
         },
         {
-          label: 'Counselor',
-          value: 'counselor',
+          label: 'Background information',
+          fields: [
+            {
+              name: 'certifications',
+              type: 'text',
+              required: false,
+            },
+            {
+              name: 'education',
+              type: 'text',
+              required: false,
+            },
+            {
+              name: 'career_history',
+              type: 'textarea',
+              required: false,
+            },
+            {
+              name: 'has_special_needs',
+              type: 'checkbox',
+              required: false,
+            },
+            {
+              name: 'is_smoker',
+              type: 'checkbox',
+              required: false,
+            },
+            {
+              name: 'has_vehicle',
+              type: 'checkbox',
+              required: false,
+            },
+            {
+              name: 'vehicle_type',
+              type: 'text',
+              required: false,
+            },
+          ],
         },
         {
-          label: 'Support',
-          value: 'support',
+          label: 'Experience Personalization',
+          fields: [
+            {
+              name: 'bio',
+              type: 'textarea',
+              required: false,
+            },
+            {
+              name: 'native_language',
+              type: 'relationship',
+              relationTo: 'languages',
+              hasMany: false,
+              required: false,
+            },
+            {
+              name: 'spoken_languages',
+              type: 'relationship',
+              relationTo: 'languages',
+              hasMany: true,
+              required: false,
+            },
+            {
+              name: 'keywords',
+              type: 'text',
+              required: false,
+            },
+          ],
+        },
+        {
+          label: 'payment information',
+          fields: [
+            {
+              name: 'stripeCustomerID',
+              label: 'Stripe Customer',
+              type: 'text',
+              access: {
+                read: ({ req: { user } }) => checkRole(['admin'], user),
+              },
+              admin: {
+                position: 'sidebar',
+                components: {
+                  Field: CustomerSelect,
+                },
+              },
+            },
+            {
+              label: 'Cart',
+              name: 'cart',
+              type: 'group',
+              fields: [
+                {
+                  name: 'items',
+                  label: 'Items',
+                  type: 'array',
+                  interfaceName: 'CartItems',
+                  fields: [
+                    {
+                      name: 'product',
+                      type: 'relationship',
+                      relationTo: 'products',
+                    },
+                    {
+                      name: 'quantity',
+                      type: 'number',
+                      min: 0,
+                      admin: {
+                        step: 1,
+                      },
+                    },
+                  ],
+                },
+                {
+                  name: 'createdOn',
+                  label: 'Created On',
+                  type: 'date',
+                  admin: {
+                    readOnly: true,
+                  },
+                },
+                {
+                  name: 'lastModified',
+                  label: 'Last Modified',
+                  type: 'date',
+                  admin: {
+                    readOnly: true,
+                  },
+                },
+              ],
+            },
+          ],
         },
       ],
-      hooks: {
-        beforeChange: [ensureFirstUserIsAdmin],
-      },
-      access: {
-        read: admins,
-        create: admins,
-        update: admins,
-      },
-    },
-    {
-      name: 'dob',
-      type: 'date',
-      required: true,
-    },
-    {
-      name: 'phoneCode',
-      type: 'number',
-      required: true,
-    },
-    {
-      name: 'phoneNumber',
-      type: 'text',
-      required: true,
-    },
-    {
-      name: 'profile_img',
-      type: 'relationship',
-      relationTo: 'media',
-      required: true,
-    },
-    {
-      name: 'gender',
-      type: 'radio',
-      options: [
-        {
-          label: 'Male',
-          value: 'male',
-        },
-        {
-          label: 'Female',
-          value: 'female',
-        },
-        {
-          label: 'Other',
-          value: 'other',
-        },
-      ],
-      required: true,
-    },
-    {
-      name: 'certifications',
-      type: 'text',
-      required: false,
-    },
-    {
-      name: 'education',
-      type: 'text',
-      required: false,
-    },
-    {
-      name: 'career_history',
-      type: 'textarea',
-      required: false,
-    },
-    {
-      name: 'has_vehicle',
-      type: 'checkbox',
-      required: false,
-    },
-    {
-      name: 'vehicle_type',
-      type: 'text',
-      required: false,
-    },
-    {
-      name: 'city',
-      type: 'text',
-      required: false,
-    },
-    {
-      name: 'years_in_city',
-      type: 'number',
-      required: false,
-    },
-    {
-      name: 'bio',
-      type: 'textarea',
-      required: false,
-    },
-    {
-      name: 'has_special_needs',
-      type: 'checkbox',
-      required: false,
-    },
-    {
-      name: 'is_smoker',
-      type: 'checkbox',
-      required: false,
-    },
-    {
-      name: 'facebook',
-      type: 'text',
-      required: false,
-    },
-    {
-      name: 'twitter',
-      type: 'text',
-      required: false,
-    },
-    {
-      name: 'instagram',
-      type: 'text',
-      required: false,
-    },
-    {
-      name: 'linkedIn',
-      type: 'text',
-      required: false,
-    },
-    {
-      name: 'native_language',
-      type: 'relationship',
-      relationTo: 'languages',
-      hasMany: false,
-      required: false,
-    },
-    {
-      name: 'spoken_languages',
-      type: 'relationship',
-      relationTo: 'languages',
-      hasMany: true,
-      required: false,
-    },
-    {
-      name: 'keywords',
-      type: 'text',
-      required: false,
-    },
-    {
-      name: 'nationality',
-      type: 'relationship',
-      relationTo: 'countries',
-      hasMany: false,
-      required: false,
     },
     {
       name: 'purchases',
@@ -240,67 +332,6 @@ const Users: CollectionConfig = {
       hooks: {
         beforeChange: [resolveDuplicatePurchases],
       },
-    },
-    {
-      name: 'stripeCustomerID',
-      label: 'Stripe Customer',
-      type: 'text',
-      access: {
-        read: ({ req: { user } }) => checkRole(['admin'], user),
-      },
-      admin: {
-        position: 'sidebar',
-        components: {
-          Field: CustomerSelect,
-        },
-      },
-    },
-    {
-      label: 'Cart',
-      name: 'cart',
-      type: 'group',
-      fields: [
-        {
-          name: 'items',
-          label: 'Items',
-          type: 'array',
-          interfaceName: 'CartItems',
-          fields: [
-            {
-              name: 'product',
-              type: 'relationship',
-              relationTo: 'products',
-            },
-            {
-              name: 'quantity',
-              type: 'number',
-              min: 0,
-              admin: {
-                step: 1,
-              },
-            },
-          ],
-        },
-        // If you wanted to maintain a 'created on'
-        // or 'last modified' date for the cart
-        // you could do so here:
-        // {
-        //   name: 'createdOn',
-        //   label: 'Created On',
-        //   type: 'date',
-        //   admin: {
-        //     readOnly: true
-        //   }
-        // },
-        // {
-        //   name: 'lastModified',
-        //   label: 'Last Modified',
-        //   type: 'date',
-        //   admin: {
-        //     readOnly: true
-        //   }
-        // },
-      ],
     },
     {
       name: 'skipSync',
